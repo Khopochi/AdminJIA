35f2beab103bf35a2bfab3e09ad6f4788dd9dda2
Khopochi <khopochirwa@gmail.com>
2024-04-13 10:59:40 +0200

    main

--- src/mypackets/table/TableView.jsx
+++ src/mypackets/table/TableView.jsx
@@ -12,6 +12,16 @@ export const TableView = () => {
   const [subcategories, setSubCategories] = useState({ id: 1, name: 'Loading...' })
   const [deepcategories,  setDeepCategories] = useState({ id: 1, name: 'Loading...' })
   const [products,  setProducts] = useState({ id: 1, name: 'Loading...' })
+
+
+  let proallProducts = sessionStorage.getItem('proallproducts');
+  let ccat = sessionStorage.getItem('catallproducts');
+  let ssubcat = sessionStorage.getItem('suballproducts');
+  let deepdeep = sessionStorage.getItem('deepallproducts');
+
+  
+
+
   const fetchData = async () => {
     try {
       // Use Promise.all to perform multiple asynchronous actions concurrently
@@ -21,12 +31,36 @@ export const TableView = () => {
         axios.get(baseurl + "subcategory/"),
         axios.get(baseurl + "deepcategory/"),
       ]);
+
+
   
       // Set state after all asynchronous actions are completed
       setProducts(product.data);
+      let allProducts = product.data
+      sessionStorage.setItem('proallproducts', JSON.stringify(product.data));
+
       setCategories(cat.data);
+      sessionStorage.setItem('catallproducts', JSON.stringify(cat.data));
+
       setSubCategories(subcat.data);
+      sessionStorage.setItem('suballproducts', JSON.stringify(subcat.data));
+
       setDeepCategories(deepcat.data);
+      sessionStorage.setItem('deepallproducts', JSON.stringify(deepcat.data));
+
+
+      // Fetch additional products in chunks of 3000 until there are no more products
+      const additionalProducts = await axios.get(baseurl + "product/again");
+      const addData = additionalProducts.data
+      const fada = allProducts.concat(addData)
+
+      console.log(fada)
+
+
+      // Update state and session storage with the new products
+      setProducts(fada);
+      sessionStorage.setItem('proallproducts', JSON.stringify(fada));
+
     } catch (error) {
       // Handle errors if any of the requests fail
       console.error("Error fetching data:", error);
@@ -34,8 +68,17 @@ export const TableView = () => {
   };
   
   useEffect(() => {
-    fetchData();
+    if(proallProducts, ccat, ssubcat, deepdeep){
+      setProducts(JSON.parse(proallProducts));
+      setDeepCategories(JSON.parse(deepdeep));
+      setSubCategories(JSON.parse(ssubcat));
+      setCategories(JSON.parse(ccat));
+    }else{
+      fetchData();
+    }
   }, []);
+
+  // console.log(products, deepcategories, subcategories, categories)
   
   //True or False on what to display
   const [cat,setcat] = useState(false)
@@ -76,10 +119,10 @@ export const TableView = () => {
             <div  onClick={()=>choose("deep")} className={`products ${deepcat ? "active" : ""}`}>DeepCategories</div>
         </div>
         <div className="lower">
-            {product && <DataTable rowss={products}/>}
-            {cat && <CategoryTable rowss={categories}/>}
-            {subcat && <SubCategoryTable rowss={subcategories}/>}
-            {deepcat && <DeepCategoryTable rowss={deepcategories}/>}
+            {(product && products && categories && subcategories && deepcategories) && <DataTable rowss={products} cat={categories} subcat={subcategories} deepcat={deepcategories}/>}
+            {(cat && products && categories && subcategories && deepcategories) && <CategoryTable rowss={categories}/>}
+            {(subcat && products && categories && subcategories && deepcategories) && <SubCategoryTable rowss={subcategories} cat={categories}/>}
+            {(deepcat && products && categories && subcategories && deepcategories) && <DeepCategoryTable rowss={deepcategories} cat={categories} subcat={subcategories}/>}
         </div>
     </div>
   )
